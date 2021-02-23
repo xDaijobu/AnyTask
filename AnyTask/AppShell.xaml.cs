292500db92876ee85da66d7fe4c6245f207fc84a
xDaijobu <christwurangian@gmail.com>
2021-02-23 14:09:16 +0700

    bersih2

--- AnyTask/AppShell.xaml.cs
+++ AnyTask/AppShell.xaml.cs
@@ -11,8 +11,7 @@ namespace AnyTask
         public AppShell()
         {
             InitializeComponent();
-            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
-            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+            Routing.RegisterRoute(nameof(DummyPage), typeof(DummyPage));
         }
 
     }
---
+++ AnyTask/Views/DummyPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace AnyTask.Views
+{
+    public partial class DummyPage : ContentPage
+    {
+        public DummyPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
